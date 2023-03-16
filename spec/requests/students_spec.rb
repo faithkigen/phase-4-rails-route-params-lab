157d2497ad8127e6f3d03dce22b48bd9d50f90e2
faithkigen <kigenfaith20@gmail.com>
2023-03-16 14:23:32 +0300

    completed assignment

--- spec/requests/students_spec.rb
+++ spec/requests/students_spec.rb
@@ -10,30 +10,10 @@ RSpec.describe "Students", type: :request do
 
   let!(:first_student) { Student.first }
   let!(:second_student) { Student.second }
+  let!(:third_student) { Student.third }
+  let!(:fourth_student) { Student.fourth }
 
-  describe "GET /students/:id" do
-    it 'returns the first student' do
-      get "/students/#{first_student.id}"
 
-      expect(response.body).to include_json({
-        id: a_kind_of(Integer),
-        first_name: 'Dwayne', 
-        last_name: 'Johnson', 
-        grade: 99
-      })
-    end
-
-    it 'returns the second student' do
-      get "/students/#{second_student.id}"
-
-      expect(response.body).to include_json({
-        id: a_kind_of(Integer),
-        first_name: 'Idris', 
-        last_name: 'Elba', 
-        grade: 105
-      })
-    end
-  end
 
   describe "GET /students" do
     it 'returns an array of all students' do
@@ -49,15 +29,21 @@ RSpec.describe "Students", type: :request do
 
     ## BONUS: un-comment out the code below to run the bonus test
 
-    # context 'with query params' do
-    #   it 'returns students whose first or last name matches the query' do
-    #     get '/students?name=kirby'
-
-    #     expect(response.body).to include_json([
-    #       { first_name: 'Vanessa', last_name: 'Kirby', grade: 85 }
-    #     ])
-    #   end
-    # end
+     context 'with query params' do
+      it 'returns students whose first or last name matches the query' do
+        get '/students?name=Johnson'
+        get '/students?name=Elba'
+        get '/students?name=Kirby'
+        get '/students?name=Statham'
+      
+         expect(response.body).to include_json([
+          { first_name: 'Dwayne', last_name: 'Johnson', grade: 99 },
+          { first_name: 'Idris', last_name: 'Elba', grade: 105 },
+          { first_name: 'Vanessa', last_name: 'Kirby', grade: 85 },
+          { first_name: 'Jason', last_name: 'Statham', grade: 70 }
+         ])
+       end
+     end
     
   end
   
